98bc19d93fe59d180508e00efc96becc17ee2e13
Pedro Ivo <cinelli.dev@gmail.com>
2022-03-22 11:03:13 -0300

    Estruturacao inicial do projeto

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import Header from '../Header/'
+
+export default function App() {
+  return (
+    <>
+    <Header />
+    <h1>Hello World</h1>
+    </>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import reactDOM from 'react-dom';
+import React from 'react'
+
+import "./assets/css/style.css"
+import "./assets/css/reset.css"
+
+import App from './components/App';
+
+reactDOM.render(<App />, document.querySelector(".root"))
